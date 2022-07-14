642d62570d29bf736f74b5553ef452fe01c119c4
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 14:39:52 +0200

    add users table

--- migrations/sqls/20220714123709-users-up.sql
+++ migrations/sqls/20220714123709-users-up.sql
@@ -1,1 +1,8 @@
 /* Replace with your SQL commands */
+CREATE TABLE users (
+    id SERIAL PRIMARY  KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    password VARCHAR
+    token VARCHAR
+);
