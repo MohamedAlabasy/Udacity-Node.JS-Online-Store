493f1201484fb556ac0c3045ac2e6d4a858c550c
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 15:56:45 +0200

    update orders migrations

--- migrations/sqls/20220714125859-orders-up.sql
+++ migrations/sqls/20220714125859-orders-up.sql
@@ -1,6 +1,8 @@
 /* Replace with your SQL commands */
+CREATE TYPE statusENUM AS ENUM ('active','complete');
+
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    status enum('active' or 'complete'),
+    status statusENUM default 'active',
     quantity integer NOT NULL
 );
