389cc17ed03854db5f4ac5ecb2283708104f30b5
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 19:21:55 +0200

    make email UNIQUE

--- migrations/sqls/20220714141114-create-users-table-up.sql
+++ migrations/sqls/20220714141114-create-users-table-up.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     password VARCHAR NOT NULL,
