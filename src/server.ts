706bc6f76b021885f3b36bb3e24e02e7ca2a9ce5
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 14:21:34 +0200

    add morgan Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,26 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
 import 'dotenv/config'
+import body_parser from 'body-parser';
 import cors from 'cors';
 
 
-const app: express.Application = express()
-// const address: string = "0.0.0.0:3000"
 
-// app.use(bodyParser.json())
-
-// app.get('/', function (req: Request, res: Response) {
-//     res.send('Hello World!')
-// })
-
+const app = express();
+// #=======================================================================================#
+// #			                        connect mongoose                                   #
+// #=======================================================================================#
 app.listen(process.env.PORT || 8888, () => {
     console.log(`App Run to http://${process.env.HOST}:${process.env.PORT || 8888}`);
 });
+// #=======================================================================================#
+// #			                            body_parse                                     #
+// #=======================================================================================#
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+// #=======================================================================================#
+// #			                     add header or use cors                                #
+// #=======================================================================================#
+// app.use(headerAccessMiddleware);
+app.use(cors());
+
+export default app;
