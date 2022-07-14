7969730d4599e405805b1595d93a01f5f7f87a0b
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 14:19:31 +0200

    start with projects

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import 'dotenv/config'
+import cors from 'cors';
+
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+// const address: string = "0.0.0.0:3000"
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+// app.get('/', function (req: Request, res: Response) {
+//     res.send('Hello World!')
+// })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+app.listen(process.env.PORT || 8888, () => {
+    console.log(`App Run to http://${process.env.HOST}:${process.env.PORT || 8888}`);
+});
