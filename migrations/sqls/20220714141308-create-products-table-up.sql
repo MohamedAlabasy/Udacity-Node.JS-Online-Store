5175946e976be5708e9c72d13cd0483bed5e1ea4
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-14 16:34:06 +0200

    add FOREIGN KEY to tables

--- migrations/sqls/20220714141308-create-products-table-up.sql
+++ migrations/sqls/20220714141308-create-products-table-up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
-    price integer NOT NULL
+    price integer NOT NULL,
+    category_id bigint REFERENCES categories(id) NOT NULL
 );
