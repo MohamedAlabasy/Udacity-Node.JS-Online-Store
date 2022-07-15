693ff4e23119359f1442d1eec7fa8675252fc960
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-15 13:48:01 +0200

    add order_products table

--- migrations/sqls/20220714141449-create-orders-table-up.sql
+++ migrations/sqls/20220714141449-create-orders-table-up.sql
@@ -4,7 +4,6 @@ CREATE TYPE statusENUM AS ENUM ('active','complete');
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     status statusENUM default 'active',
-    quantity integer NOT NULL,
-    product_id bigint REFERENCES products(id) NOT NULL,
     user_id bigint REFERENCES users(id) NOT NULL
 );
+
